f7a14f01e12b0b042f7e2f4110e9f7efe7b34bbb
baljinder <bawabaljinder00@gmail.com>
2025-08-26 14:39:57 +0530

    fix

--- vars/push.groovy
+++ vars/push.groovy
@@ -4,5 +4,5 @@ def call(String repo, String image, String version ){
                               passwordVariable:"pass")])
             sh "docker tag myapp01 ${env.user}/test01:v3"
             sh "echo ${env.pass} | docker login -u ${env.user} --password-stdin"
-            sh "docker push ${repo}/${image}:${version}
+            sh "docker push ${repo}/${image}:${version}"
             {                        
