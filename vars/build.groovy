20ed1beb9d639fbb7ef3c6e6de275495f3fc5452
baljinder <bawabaljinder00@gmail.com>
2025-08-26 14:37:25 +0530

    fix

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,5 @@
 def call(String app12, String username , String reponame , String version){ 
-sh "docker build -t ${app12} i."
+sh "docker build -t ${app12} ."
 sh " docker tag ${app12} ${username}/${reponame}:${version}"
 
 
--- vars/push.groovy
+++ vars/push.groovy
@@ -1,9 +1,8 @@
-def call(String repo, String image, String version ){
-    ll
+def call(String repo, String image, String version ){ 
      withCredentials([usernamePassword("credentialsId":"dockerhub",
                               usernameVariable:"user",
                               passwordVariable:"pass")])
             sh "docker tag myapp01 ${env.user}/test01:v3"
             sh "echo ${env.pass} | docker login -u ${env.user} --password-stdin"
-            sh "docker push "${repo}"/"${image}":"${version}"
-            }                          
+            sh "docker push ${repo}/${image}:${version}
+            {                        
