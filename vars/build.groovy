afb0fdf24d7e0ccfbbf9fabf7bc8d58618209a83
baljinder <bawabaljinder00@gmail.com>
2025-08-26 14:26:10 +0530

    solve syntax error

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
-def call(String app12, String username , String reponame , String "version" ){
+def call(String app12, String username , String reponame , String version){
 docker build -t "${app12}" .
 docker tag ${app12} ${username}/${reponame}:${version}
 
