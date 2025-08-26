5351f8ba00ad456690ce071c01395bffdb74a84e
baljinder <bawabaljinder00@gmail.com>
2025-08-26 12:48:24 +0530

    test groovy

---
+++ vars/build.groovy
@@ -0,0 +1,6 @@
+def call(String app12, String username , String reponame , String "version" ){
+docker build -t "${app12}" .
+docker tag "${app12}" "${username}"/"${reponame}":"${version}"
+
+
+}
---
+++ vars/push.groovy
@@ -0,0 +1,8 @@
+def call(String repo, String image, String version ){
+     withCredentials([usernamePassword("credentialsId":"dockerhub",
+                              usernameVariable:"user",
+                              passwordVariable:"pass")])
+            sh "docker tag myapp01 ${env.user}/test01:v3"
+            sh "echo ${env.pass} | docker login -u ${env.user} --password-stdin"
+            sh "docker push "${repo}"/"${image}":"${version}"
+            }                          
